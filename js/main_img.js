cfab0381ab66ec18fcae9edc04dce7b5d74c2bde
jonghyeon <cjddnjfql@naver.com>
2022-01-26 23:33:26 +0900

    notice수정중

---
+++ js/main_img.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  setTimeout(() => {
+    document.querySelector(".main_slogan").style.opacity = "1";
+  }, 500);
+  setTimeout(() => {
+    document.querySelector(".main_img01").style.opacity = "1";
+  }, 1000);
+  setTimeout(() => {
+    document.querySelector(".main_img02").style.opacity = "1";
+  }, 1500);
+  setTimeout(() => {
+    document.querySelector(".main_img03").style.opacity = "1";
+  }, 2000);
+  setTimeout(() => {
+    document.querySelector(".slogan_bt").style.opacity = "1";
+  }, 2500);
+});
