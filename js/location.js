77a8950e2e2cfff274a847a7142bb49bd935b0de
jonghyeon <cjddnjfql@naver.com>
2022-02-09 13:30:40 +0900

    ajax 수정

--- js/location.js
+++ js/location.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   const map = document.querySelectorAll(".gnb_top > ul > li");
+  const logo = document.querySelector(".logo a");
+  $(logo).click(function (e) {
+    location.href = "index.html";
+  });
   $(map[3]).click(function (e) {
     location.href = "map.html";
   });
